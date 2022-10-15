5639b5d56a4910272351dd3f9ac185c909d2ef10
kevin daniliuk <108556672+DaniliukK95@users.noreply.github.com>
2022-10-15 16:28:43 -0400

    finished app.js for module 11.5

--- static/js/app.js
+++ static/js/app.js
@@ -27,3 +27,31 @@ function buildTable(data) {
         );
     });
 }
+
+
+//code to filter data based on date. 
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}
+
+
+//code for when the click happens. attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//build the table when the page loads.
+buildTable(tableData);
